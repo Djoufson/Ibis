f5abd59e02b72ace955f6bcb1f38f975275c3226
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-21 11:55:28 +0100

    feat: Create a new weather_type
    The weather types are stored in a database, and this feature is meant to populate this database with new weather_types

---
+++ controllers/weather_type_controller.py
@@ -0,0 +1,9 @@
+from utils.configure import app
+from services.weather_type_service import add_weather
+from flask import request, jsonify
+
+@app.route('/weather_types', methods=['POST'])
+def add_weather_type():
+    data = request.get_json()
+    result = add_weather(data)
+    return jsonify(result)
