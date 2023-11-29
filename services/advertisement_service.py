ff3a5fb0d233cdef9b49357aa7281b47c66bc372
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-29 14:36:24 +0100

    fix: Fix the openAI API call issue

--- services/advertisement_service.py
+++ services/advertisement_service.py
@@ -1,9 +1,33 @@
 from models.weather_type import WeatherType
 from models.product import Product
-from openai_service import generate_openai_advertisement
 import random
+from openai import OpenAI
+from dotenv import load_dotenv
 
-def generate_advertisement(weather_id:int) -> tuple[dict[str, str], int]:
+load_dotenv()
+client = OpenAI()
+
+def generate_openai_advertisement(prompt:str, max_tokens=100) -> str:
+    try:
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+
+            model="gpt-3.5-turbo",
+            max_tokens=max_tokens
+        ).choices[0].message.content
+    except Exception as e:
+        print(e)
+        return None
+
+    return response
+
+
+def generate_advertisement(weather_id:int, max_tokens:int) -> tuple[dict[str, str], int]:
     weather_type = WeatherType.query.get(weather_id)
 
     if not weather_type:
@@ -15,7 +39,10 @@ def generate_advertisement(weather_id:int) -> tuple[dict[str, str], int]:
         return {'error': 'No product found for this weather'}, 404
 
     chosen_product = random.choice(products)
-    prompt = f'Introducing our {chosen_product.name} for {weather_type.type_name} weather!'
-    advertisement_text = generate_openai_advertisement(prompt)
+    prompt = f"Introducing our product named '{chosen_product.name}' for {weather_type.type_name} weather!"
+
+    advertisement_text = generate_openai_advertisement(prompt, max_tokens)
+    if advertisement_text == None:
+        advertisement_text = chosen_product.name
 
     return chosen_product.name, advertisement_text
