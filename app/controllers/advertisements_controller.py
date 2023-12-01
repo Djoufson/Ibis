beaca6621f7796369da764c30cdbc953868e6380
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-01 05:21:38 +0100

    feat: Enable the advertisement genera

--- app/controllers/advertisements_controller.py
+++ app/controllers/advertisements_controller.py
@@ -1,8 +1,11 @@
 from flask import jsonify
 from app.utils.configure import app
 from app.services.advertisement_service import generate_advertisement
+from flask import request
 
 @app.route('/advertisements/<int:weather_id>/<int:max_tokens>', methods=['GET'])
 def get_advertisement(weather_id, max_tokens):
-    product, advertisement_text = generate_advertisement(weather_id, max_tokens)
+    # Get language from query param
+    lang = request.args.get('lang')
+    product, advertisement_text = generate_advertisement(weather_id, lang, max_tokens)
     return jsonify({'product': product, 'advertisement_text': advertisement_text})
--- app/services/advertisement_service.py
+++ app/services/advertisement_service.py
@@ -27,7 +27,7 @@ def generate_openai_advertisement(prompt:str, max_tokens=100) -> str:
     return response
 
 
-def generate_advertisement(weather_id:int, max_tokens:int) -> tuple[dict[str, str], int]:
+def generate_advertisement(weather_id:int, lang:str = 'en', max_tokens:int = 30) -> tuple[dict[str, str], int]:
     weather_type = WeatherType.query.get(weather_id)
 
     if not weather_type:
@@ -39,7 +39,10 @@ def generate_advertisement(weather_id:int, max_tokens:int) -> tuple[dict[str, st
         return {'error': 'No product found for this weather'}, 404
 
     chosen_product = random.choice(products)
-    prompt = f"Introducing our product named '{chosen_product.name}' for {weather_type.type_name} weather!"
+    if lang == 'en':
+        prompt = f"Introducing our product named '{chosen_product.name}' for {weather_type.type_name} weather! Make an advertisement of maximum {max_tokens} tokens"
+    else:
+        prompt = f"Rédige une publicité accrocheuse du produit '{chosen_product.name}' adapté à un climat {weather_type.type_name} de maximum {max_tokens} tokens"
 
     advertisement_text = generate_openai_advertisement(prompt, max_tokens)
     if advertisement_text == None:
